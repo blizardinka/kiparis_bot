7fd281f5cc39691bedc54841a29abf82735b96f3
blizardinka <serhiichornoivan@MBP-blizardinka.fritz.box>
2020-11-23 14:36:51 +0100

    change java version and add new imports

---
+++ src/main/java/kom/kiparis/Bot.java
@@ -0,0 +1,37 @@
+package kom.kiparis;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "jung_kiparis_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1427109144:AAFvRTCjSId9i-3m90SLBIzvblBOQf3vQeU";
+    }
+}
