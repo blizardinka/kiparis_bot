896a24afe5dbeee46fc7a7c8eab736a76adeaf2e
Serhii Chornoivan <schornoivan@Serhiis-iMac-Pro.local>
2020-12-03 23:47:48 +0100

    change procfile, pom and main.java

--- src/main/java/org/kiparis/Main.java
+++ src/main/java/org/kiparis/Main.java
@@ -4,12 +4,24 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+    private static final String PORT = System.getenv("PORT");
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new Bot());
         } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        try (ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT))) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
